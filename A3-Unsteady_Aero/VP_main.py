83feb7d1221de98bd734fd82fb4820b7edfd5664
mathieupelle <mathieu.pelle7@gmail.com>
2021-06-26 09:21:09 +0200

    Fixed contours. NC lift.

--- A3-Unsteady_Aero/VP_main.py
+++ A3-Unsteady_Aero/VP_main.py
@@ -11,38 +11,42 @@ from VP_plotting import steady_polars, steady_contours, scatter, unsteady_polars
 from VP_utilities import vortex_panel
 
 
-#%% Steady case - Polar, pressure and velocity contours
 
-#Polars
-alpha = np.arange(-15,15,1)
+#%% Steady case - Polars
+
+alpha = np.arange(-20,20,2)
 results = []
 for i in range(len(alpha)):
-    result = vortex_panel([0], 10, [alpha[i]], [0], U_inf=2)
+    result = vortex_panel([0], 10, [alpha[i]], [0])
     results.append(result)
 
 steady_polars(alpha, results)
 
-#Contours
-result = vortex_panel([0], 10, [30], [0], U_inf=1)
-steady_contours(alpha, result, quiver=False, streamlines=True)
+#%% Steady case - Contours
+
+result = vortex_panel([0], 30, [15], [0], U_inf=1, c=1)
+scatter([result])
+steady_contours(alpha, result, streamlines=True)
 
 #%% Unsteady case - oscillations
 
-k=0.02
+k=0.01
 U_inf = 5
-c = 0.1
+c = 0.05
 omega=k*2*U_inf/c
 T = 2*np.pi/omega
-t = np.hstack((np.linspace(0, T, 40),np.linspace(0, T, 40)))
+N = 81
+dt = 2*T/N
+t = np.linspace(0,2*T, N)
 
-theta = 10+10*np.sin(omega*t)
+theta = 10+5*np.sin(omega*t)
 theta_dot = omega*np.cos(omega*t)
 
 result = vortex_panel(t, 10, theta, theta_dot, c=c, U_inf=U_inf)
 
 #scatter([result])
 
-unsteady_polars(theta, result, basic_method=False)
+unsteady_polars(theta, result, basic_method=True, inputs=True)
 
 #%% Unsteady case - step
 
@@ -58,13 +62,67 @@ unsteady_polars(theta, result, basic_method=False)
 
 #%% Steady case - flap
 
-flap = {'length':0.1, 'angle':10, 'N_panels':2}
-
+flap_angle = np.linspace(0,45,10)
 alpha = np.arange(0,15,1)
-results = []
-for i in range(len(alpha)):
-    result = vortex_panel([0], 10, [alpha[i]], [0], U_inf=2, flap=flap)
-    results.append(result)
-    #scatter([result])
+flap_results = {}
 
-steady_polars(alpha, results, moment=False, flap=flap)
+for j in range(len(flap_angle)):
+    flap = {'length':0.2, 'angle':flap_angle[j], 'N_panels':5}
+    results = []
+    for i in range(len(alpha)):
+        result = vortex_panel([0], 10, [alpha[i]], [0], c=0.8, U_inf=2, flap=flap)
+        results.append(result)
+    flap_results['flap_angle'+str(flap_angle[j])] = results
+
+
+flap_length = np.linspace(0.1,1,10)
+alpha = np.arange(0,15,1)
+flap_results = {}
+
+for j in range(len(flap_length)):
+    flap = {'length':flap_length[j], 'angle':10, 'N_panels':5}
+    results = []
+    for i in range(len(alpha)):
+        result = vortex_panel([0], 10, [alpha[i]], [0], c=1, U_inf=2, flap=flap)
+        results.append(result)
+    flap_results['flap_length'+str(flap_length[j])] = results
+
+#%%
+import matplotlib.pyplot as plt
+
+
+def flap_analysis(flap_results, flap, alpha, parameter, parameter_lst, contour=False, rho=1.225):
+    Cl = np.zeros((len(parameter_lst),len(alpha)))
+    for j in range(len(parameter_lst)):
+        results = flap_results[parameter+str(parameter_lst[j])]
+        for i in range(len(alpha)):
+            result = results[i]
+            c = result['chord']+flap['length']
+            U_inf = np.linalg.norm(result['velocity'])
+            dL = rho*U_inf*(result['gamma'][0])
+            L = sum(dL)
+            Cl[j,i] = L/(0.5*rho*U_inf**2*c)
+
+        if j%2==0:
+            plt.plot(alpha, Cl[j,:], label=r'$\beta$='+str(round(parameter_lst[j]))+'$^\circ$')
+        plt.legend()
+        plt.xlabel(r'$\alpha$ [$^\circ$]')
+        plt.ylabel('$C_l$ [-]')
+        plt.grid()
+
+
+    if contour:
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+        X = alpha
+        Y = parameter_lst
+        X, Y = np.meshgrid(X, Y)
+
+        ax.plot_surface(X, Y, Cl, cmap='viridis')
+        ax.set_ylabel(r'$\beta$ [$^\circ$]')
+        ax.set_xlabel(r'$\alpha$ [$^\circ$]')
+        ax.set_zlabel('$C_l$ [-]')
+
+
+#flap_analysis(flap_results, flap, alpha, 'flap_angle', flap_angle, rho=1.225)
+flap_analysis(flap_results, flap, alpha, 'flap_length', flap_length, rho=1.225)
--- A3-Unsteady_Aero/VP_plotting.py
+++ A3-Unsteady_Aero/VP_plotting.py
@@ -28,7 +28,13 @@ def steady_polars(alpha, results, rho=1.225, moment=True, flap=False):
 
             if j==0:
                 C[i] = L/(0.5*rho*U_inf**2*c)
-                C_theory[i]=2*np.pi*np.sin(np.deg2rad(alpha[i]))
+                if flap:
+                    hinge = result['chord']/(result['chord']+flap['length'])
+                    theta_k=np.arccos(1-2*hinge)
+                    C_theory[i] = 2*np.pi*np.sin(np.deg2rad(alpha[i]+flap['angle']*((1-theta_k/np.pi)+1/np.pi*np.sin(theta_k))))
+
+                else:
+                    C_theory[i]=2*np.pi*np.sin(np.deg2rad(alpha[i]))
                 label = '$C_l$ [-]'
             else:
                 x_lst = []
@@ -42,70 +48,80 @@ def steady_polars(alpha, results, rho=1.225, moment=True, flap=False):
 
 
         plt.figure()
-        plt.plot(alpha, C, label='Vortex panel code')
-        plt.plot(alpha, C_theory, 'x', label='2D Airfoil theory')
+        plt.plot(alpha, C_theory, 'o', label='2D Airfoil theory')
+        plt.plot(alpha, C,'-k', label='Vortex panel code')
         plt.grid()
         plt.xlabel(r'$\alpha$ [$^{\circ}$]')
         plt.ylabel(label)
         plt.legend()
 
 
-def steady_contours(alpha, result, rho=1.225, quiver=True, streamlines=True):
+def steady_contours(alpha, result, rho=1.225, streamlines=True):
     c = result['chord']
     TE = result['TE'][0]
     LE = result['LE'][0]
-
+    u_inf = np.linalg.norm(result['velocity'])
     #For contours
-    x = np.linspace(-1.5*c,1.5*c,20)
-    xx, zz = np.meshgrid(x,x)
-    V_mag = np.zeros((len(x), len(x)))
-    p = np.zeros((len(x), len(x)))
-    for i in range(len(x)):
+
+    #Finer mesh - not really working
+    # x_pre = np.linspace(-1.5*c,LE[0,0],20)
+    # x_aft = np.linspace(TE[0,0],1.5*c,10)
+    # x_finer = np.linspace(LE[0,0],TE[0,0],5)
+    # x = np.hstack((x_pre, x_finer, x_aft))
+    # z_pre = np.linspace(-1.5*c,TE[1,0],20)
+    # z_aft = np.linspace(LE[1,0],1.5*c,10)
+    # z_finer = np.linspace(TE[1,0],LE[1,0],5)
+    # z = np.hstack((z_pre, z_finer, z_aft))
+
+    x = np.linspace(-1.5*c,1.5*c,32)
+    z = x
+    xx, zz = np.meshgrid(z,x)
+    V_mag = np.zeros((len(z), len(x)))
+    p = np.zeros((len(z), len(x)))
+    U = np.zeros((len(z), len(x)))
+    V = np.zeros((len(z), len(x)))
+    for i in range(len(z)):
         for j in range(len(x)):
             v = np.array([[0], [0]])
             for k in range(len(result['vortices'][0])):
-                v = v + VOR2D(np.array([[x[i]], [x[j]]]), result['vortices'][0][k], result['gamma'][0][k,0])
+                v = v + VOR2D(np.array([[z[i]], [x[j]]]), result['vortices'][0][k], result['gamma'][0][k,0])
             vel = v+result['velocity']
-            V_mag[i, j] = np.sqrt(vel[0]**2+vel[1]**2)
-            p[i, j] = 0.5*rho*(-V_mag[i, j]**2+np.linalg.norm(result['velocity'])**2)
+            V_mag[j, i] = np.sqrt(vel[0]**2+vel[1]**2)
+            U[j, i] = vel[0,0]
+            V[j, i] = vel[1,0]
+            p[j, i] = 0.5*rho*(-V_mag[j, i]**2+u_inf**2)
 
     plt.figure()
-    cp = plt.contourf(xx, zz, V_mag, 150, cmap='jet')
-
-    #For quiver
-    x = np.linspace(-1.5*c, 1.5*c, 20)
-    Vx = np.zeros((len(x), len(x)))
-    Vz = np.zeros((len(x), len(x)))
-    for i in range(len(x)):
-        for j in range(len(x)):
-            v = np.array([[0], [0]])
-            for k in range(len(result['vortices'][0])):
-                v = v + VOR2D(np.array([[x[i]], [x[j]]]), result['vortices'][0][k], result['gamma'][0][k,0])
-            vel = v+result['velocity']
-            Vx[i, j] = vel[0,0]
-            Vz[i, j] = vel[1,0]
-    if quiver:
-        plt.quiver(x,x,Vx,Vz, color='black')
+    cp = plt.contourf(xx/c, zz/c, U/u_inf, 200, cmap='jet')
     cbar = plt.colorbar(cp)
-    cbar.ax.set_ylabel('Velocity magnitude [m/s]', rotation=270, labelpad=15)
-    plt.ylabel('z [m]')
-    plt.xlabel('x [m]')
-    plt.plot([LE[0,0], TE[0,0]], [LE[1,0], TE[1,0]], 'k',  linewidth=3, alpha=0.6)
-    #plt.scatter(zz,xx, marker='+')
+    cbar.ax.set_ylabel('$U/U_{\infty}$ [-]', rotation=270, labelpad=15)
+    plt.ylabel('z/c [-]')
+    plt.xlabel('x/c [-]')
+    plt.plot([LE[0,0], TE[0,0]], [LE[1,0], TE[1,0]], 'k',  linewidth=3.2)
+    #plt.scatter(zz,xx, marker='+', color='white', alpha=0.5)
+
+    plt.figure()
+    cp = plt.contourf(xx/c, zz/c, V/u_inf, 200, cmap='jet')
+    cbar = plt.colorbar(cp)
+    cbar.ax.set_ylabel('$V/U_{\infty}$ [-]', rotation=270, labelpad=15)
+    plt.ylabel('z/c [-]')
+    plt.xlabel('x/c [-]')
+    plt.plot([LE[0,0], TE[0,0]], [LE[1,0], TE[1,0]], 'k',  linewidth=3.2)
+
 
     if streamlines:
         plt.figure()
         #seed_points = np.linspace((-1.5*c,-1.5*c),(-1.5*c,1.5*c),20)
-        plt.streamplot(x,x,Vx,Vz)#, start_points=seed_points)
-        plt.plot([LE[0,0], TE[0,0]], [LE[1,0], TE[1,0]], 'k',  linewidth=3, alpha=0.6)
+        plt.streamplot(x,x,U,V)#, start_points=seed_points)
+        plt.plot([LE[0,0], TE[0,0]], [LE[1,0], TE[1,0]], 'k',  linewidth=3)
 
     plt.figure()
-    cp = plt.contourf(xx, zz, p, 150, cmap='jet')
+    cp = plt.contourf(xx/c, zz/c, p/(0.5*rho*u_inf**2), 200, cmap='jet')
     cbar = plt.colorbar(cp)
-    cbar.ax.set_ylabel('Dynamic pressure [Pa]', rotation=270, labelpad=15)
-    plt.ylabel('z [m]')
-    plt.xlabel('x [m]')
-    plt.plot([LE[0,0], TE[0,0]], [LE[1,0], TE[1,0]], 'k',  linewidth=3, alpha=0.6)
+    cbar.ax.set_ylabel('$\Delta q/q_{\infty}$', rotation=270, labelpad=15)
+    plt.ylabel('z/c [-]')
+    plt.xlabel('x/c [-]')
+    plt.plot([LE[0,0], TE[0,0]], [LE[1,0], TE[1,0]], 'k',  linewidth=3.2)
 
 
 def scatter(results, TE=True, LE=True, Vortex=True):
@@ -152,11 +168,13 @@ def get_CL(result, rho, theta):
     time = result['time']
     dt = time[1]-time[0]
     Cl = np.zeros(len(time))
+    Cl_NC = np.zeros(len(time))
     Cl_theory = np.zeros(len(time))
     for t in range(len(time)):
         tan_vec = result['TE'][t]-result['LE'][t]
         tan_vec = tan_vec/np.linalg.norm(tan_vec)
         p = np.zeros(N_panels)
+        p_NC = np.zeros(N_panels)
         for j in range(N_panels):
             v=result['velocity']
             for i in range(len(result['vortices'][t])-N_panels):
@@ -165,20 +183,23 @@ def get_CL(result, rho, theta):
 
             Q = np.dot(tan_vec.T, v)
             if t==0:
-                dgamma_dt = 0 # ??? check
+                dgamma_dt = 0
             else:
                 dgamma_dt = (sum(result['gamma'][t][:N_panels])-sum(result['gamma'][t-1][:N_panels]))/dt
             p[j] = (Q*result['gamma'][t][j,0]/result['L_panels'] + dgamma_dt)*rho
+            p_NC[j] = dgamma_dt*rho
 
 
         L = sum(p*result['L_panels'])*np.cos(np.deg2rad(theta[t]))
+        L_NC = sum(p_NC*result['L_panels'])*np.cos(np.deg2rad(theta[t]))
         Cl[t] = L/(0.5*rho*(np.linalg.norm(result['velocity']))**2*result['chord'])
+        Cl_NC[t] = L_NC/(0.5*rho*(np.linalg.norm(result['velocity']))**2*result['chord'])
         Cl_theory[t]=2*np.pi*np.sin(np.deg2rad(theta[t]))
-    return Cl, Cl_theory
+    return Cl, Cl_theory, Cl_NC
 
 def unsteady_polars(theta, result, rho=1.225, basic_method=False, inputs=False, arrows=True):
 
-    Cl, Cl_theory = get_CL(result, rho, theta)
+    Cl, Cl_theory, Cl_NC = get_CL(result, rho, theta)
 
     time = result['time']
     N_panels = result['N_panels']
@@ -187,8 +208,11 @@ def unsteady_polars(theta, result, rho=1.225, basic_method=False, inputs=False, 
     idx2 = int(3*len(time)/4)
 
     plt.figure()
-    plt.plot(theta[idx:], Cl[idx:], marker='.')
-    #plt.plot(theta[idx:], Cl_theory[idx:], '--k', label='US', alpha=0.6)
+    plt.plot(theta[idx:], Cl[idx:], '--b', label='$C_l$')
+    plt.plot(theta[idx:], Cl_NC[idx:],'-k', label='$C_{l_{NC}}$')
+    plt.plot(theta[idx:], Cl[idx:]-Cl_NC[idx:],'--r', label='$C_{l_{C}}$')
+
+
     if arrows:
         p1 = (theta[idx+2], Cl[idx+2]+0.2)
         p2 = (theta[idx], Cl[idx]+0.2)
@@ -199,9 +223,7 @@ def unsteady_polars(theta, result, rho=1.225, basic_method=False, inputs=False, 
         d1=dict(color='black', shrink=0.05, width=.5, headwidth=3, headlength=4, linewidth=.2)
         plt.annotate('', xy=p1, xytext=p2, arrowprops=d1)
 
-    plt.grid()
-    plt.xlabel(r'$\alpha$ [deg]')
-    plt.ylabel('$C_l$ [-]')
+
 
     if basic_method:
         C=np.zeros(len(theta))
@@ -211,17 +233,23 @@ def unsteady_polars(theta, result, rho=1.225, basic_method=False, inputs=False, 
             L = sum(dL)
             C[i] = L/(0.5*rho*U_inf**2*result['chord'])
 
-        plt.scatter(theta[idx:], C[idx:], marker='.', color='black', label='S')
-        plt.legend()
+        plt.plot(theta[idx:], C[idx:], '--g', label='$C_{l_{qs}}$')
+    plt.grid()
+    plt.tight_layout()
+    plt.xlabel(r'$\alpha$ [deg]')
+    plt.ylabel('$C_l$ [-]')
+    plt.legend()
 
     if inputs:
         plt.figure()
-        theta_norm = theta/max(theta)
-        cl_norm = Cl/max(Cl)
-        plt.plot(result['time'][idx:], cl_norm[idx:], label='$C_l$')
-        plt.plot(result['time'][idx:], theta_norm[idx:], label=r'$\alpha$')
+        semichord = 2*result['time'][idx:]*np.linalg.norm(result['velocity'])/result['chord']
+        semichord = semichord - semichord[0]
+        plt.plot(semichord, Cl[idx:], '--b', label='$C_l$')
+        plt.plot(semichord, Cl[idx:]-Cl_NC[idx:], '-.r', label='$C_{l_{C}}$')
+        plt.plot(semichord, Cl_NC[idx:], '-k', label='$C_{l_{NC}}$')
+        plt.xlim([0,max(semichord)])
         plt.grid()
         plt.legend()
-        plt.xlabel('time [s]')
-        plt.ylabel(r'$C_l/C_{l_{max}}$, $\alpha$/$\alpha_{max}$ [-]')
+        plt.xlabel('s [-]')
+        plt.ylabel('$C_l$')
 
